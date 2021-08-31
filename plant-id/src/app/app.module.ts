2630b4dcd7a886a01959a428309d55b490fb3338
RafaelAcheSales <rafael.ache@hotmail.com>
2021-08-31 16:22:32 -0300

    outros components

--- plant-id/src/app/app.module.ts
+++ plant-id/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { PlantComponent } from './plant/plant.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    PlantComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ plant-id/src/app/plant/plant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plant',
+  templateUrl: './plant.component.html',
+  styleUrls: ['./plant.component.css']
+})
+export class PlantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
