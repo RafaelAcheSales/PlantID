a29fe372b54f3fe4bd3c79469d81d1907e70b5ef
Weslley Silva <weslleycsil@gmail.com>
2021-09-01 18:25:35 -0300

    validacoes e form

--- plant-id/src/app/app.module.ts
+++ plant-id/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,9 @@ import { MainPageComponent } from './main-page/main-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- plant-id/src/app/signup/signup.component.ts
+++ plant-id/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  formSignup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.formSignup = this.formBuilder.group({
+      email: ['', [Validators.required,Validators.email]],
+      password1: ['', [Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
+      password2: ['', [Validators.required]],
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  criarUser() {
+    if(!this.formSignup.valid){
+      alert('Formulario inválido');
+      return;
+    }
+    alert('Formulario Válido');
   }
 
 }
